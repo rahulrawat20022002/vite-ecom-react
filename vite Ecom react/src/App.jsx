e9e96c2c7a79336418208a09ebf9d33e04bb295a
rahulrawat20022002 <94779184+rahulrawat20022002@users.noreply.github.com>
2023-11-28 23:13:49 +0530

    update

--- vite Ecom react/src/App.jsx
+++ vite Ecom react/src/App.jsx
@@ -5,6 +5,9 @@ import Cart from "./pages/cart/Cart";
 import NoPage from "./pages/noPage/NoPage";
 import Dashboard from "./pages/dashboard/Dashboard";
 import MyState from "./context/MyState";
+import Login from "./pages/registration/Login";
+import SingUp from "./pages/registration/SingUp";
+import ProductInfo from "./pages/productInfo/ProductInfo";
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
             <Route path="/order" element={<Order />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signUp" element={<SingUp />} />
+            <Route path="/productinfo/:id" element={<ProductInfo />} />
             <Route path="/*" element={<NoPage />} />
           </Routes>
         </Router>
